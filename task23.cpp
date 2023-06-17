31ae6a585e9f1ff2eb512b3e7308c70a0c3310b7
Barnich-Kate <85077866+Barnich-Kate@users.noreply.github.com>
2023-06-17 11:40:01 +0300

    Create task23.cpp
    Add solution of the task

---
+++ task23.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a, b, c;
+    cout<<"Type number 1: ";
+    cin >> a;
+    cout<<"Type number 2: ";
+    cin >> b;
+    cout<<"Type number 3: ";
+    cin >> c;
+    b = a;
+    c = b;
+    a = c;
+    cout << "Number: " << a << endl;
+    cout << "Number: " << b << endl;
+    cout << "Number: " << c << endl;
+    
+    
+
+    return 0;
+}
