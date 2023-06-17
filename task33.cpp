3bbaca460a6934971e2e04372ed0d35838f6da6e
Barnich-Kate <85077866+Barnich-Kate@users.noreply.github.com>
2023-06-17 12:18:40 +0300

    Create task33.cpp
    Add the solution of the task

---
+++ task33.cpp
@@ -0,0 +1,18 @@
+
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+int main()
+{
+    float one_kg, x, y, a;
+    one_kg = a/x;
+    y = one_kg*y;
+    cout << "Cost of the one kg of sweets =  " << one_kg << endl;
+    cout << "Y kg of the sweets = " << y << endl;
+    
+    
+
+    return 0;
+}
