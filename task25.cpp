b90023f13022c273f56cb71faf425c2fa1f089b8
Barnich-Kate <85077866+Barnich-Kate@users.noreply.github.com>
2023-06-17 12:04:32 +0300

    Create task25.cpp
    Add the solution of the task

---
+++ task25.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+int main()
+{
+    int x, y;
+    cout<<"Type number: ";
+    cin >> x;
+    y = 3 * pow(x,6) - 6 * pow(x,2) - 7;
+    cout << "y is equal:  " << y << endl;
+    
+    
+
+    return 0;
+}
